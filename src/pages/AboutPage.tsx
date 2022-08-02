b91f9584c06138b770cd696821405990ca76cc0c
apisceva <aljona.gromova@gmail.com>
2022-08-02 03:30:58 -0400

    final

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function AboutPage(){
+    return (
+        <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia laboriosam porro odio, at aspernatur ex quaerat alias quasi enim vitae illo nam expedita soluta officiis. Quidem dolores qui voluptatibus quia fugit vitae architecto neque sapiente, quod aut ullam perspiciatis nisi porro delectus modi dolorem dolor asperiores provident consectetur facilis quibusdam.</div>
+    )
+}
